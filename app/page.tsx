8662be54ba0109ac8ac0ca87c7a7cd8457e2d250
YourGitHubUsername <ramuk13476@gmail.com>
2025-09-30 21:38:58 +0530

    setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+
+
+export default function Home() {
+  return (
+  <div className="">
+   <p  className="text-3xl font-bold text-indigo-500">hello fron Linkee</p> 
+   <Button variant="text" >click me</Button>
+   <Button variant="text">Button</Button>
+  </div>
+  )
+}
